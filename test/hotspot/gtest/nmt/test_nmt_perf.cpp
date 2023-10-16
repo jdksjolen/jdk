25165f4deb38d00f099d7f28e3428302a8df91a3
Johan Sjölén <johan.sjolen@oracle.com>
2023-10-16 12:39:13 +0200

    Add perf bench

---
+++ test/hotspot/gtest/nmt/test_nmt_perf.cpp
@@ -0,0 +1,38 @@
+#include "precompiled.hpp"
+
+#include "memory/allocation.hpp"
+#include "runtime/os.hpp"
+#include "services/mallocTracker.hpp"
+#include "services/virtualMemoryTracker.hpp"
+#include "unittest.hpp"
+#include <chrono>
+
+
+TEST_VM(NMTPerf, PerfTest) {
+  auto time_it = [](auto it) {
+    auto start = std::chrono::high_resolution_clock::now();
+    it();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = (end - start);
+    auto us = std::chrono::duration_cast<std::chrono::microseconds>(duration);
+    tty->print_cr("Elapsed time: %ld us", us.count());
+  };
+
+  tty->print_cr("Adding 10000 reserved adjacent regions");
+  time_it([]() {
+    address addr = 0x0;
+    size_t size = 1024;
+    for (int i = 0; i < 10000; i++) {
+      NewVirtualMemoryTracker::add_reserved_region(addr, size, CALLER_PC);
+      addr += size;
+    }
+  });
+  time_it([](){
+    address addr = 0x0;
+    size_t size = 1024;
+    for (int i = 0; i < 10000; i++) {
+      VirtualMemoryTracker::add_reserved_region(addr, size, CALLER_PC);
+      addr += size;
+    }
+  });
+}
