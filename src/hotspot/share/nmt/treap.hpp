d82588ffbb0cdfe882d787984d327b006ef7aa89
Johan Sjölén <johan.sjolen@oracle.com>
2024-02-28 16:38:23 +0100

    Treap + VMATREE

---
+++ src/hotspot/share/nmt/treap.hpp
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef SHARE_NMT_TREAP_HPP
+#define SHARE_NMT_TREAP_HPP
+
+#include <stddef.h>
+#include <stdint.h>
+
+// A Treap is a self-balanced binary tree where each node is equipped with a
+// priority. It adds the invariant that the priority of a parent P is strictly larger
+// larger than the priority of its children. When priorities are randomly
+// assigned the tree is balanced.
+// All operations are defined through merge and split, which are each other's inverse.
+// merge(left_treap, right_treap) => treap where left_treap <= right_treap
+// split(treap, key) => (left_treap, right_treap)  where left_treap <= right_treap
+// Recursion is used in these, but the depth of the call stack is the depth of
+// the tree which is O(log n) so we are safe from stack overflow.
+// There is an imperative equivalent of these two, if needed. https://www2.hawaii.edu/~nodari/teaching/f19/scribes/notes07.pdf
+template<typename K, typename V, int(*CMP)(K,K), uint64_t(*RAND)(), void* (*ALLOC)(size_t)>
+class TreapNode {
+  friend class VMATree;
+  uint64_t priority;
+  K key;
+  V value;
+  using Nd = TreapNode<K,V,CMP, RAND, ALLOC>;
+  TreapNode<K, V, CMP, RAND, ALLOC>* left;
+  TreapNode<K, V, CMP, RAND, ALLOC>* right;
+private:
+  struct pair {
+    Nd* left;
+    Nd* right;
+  };
+
+  enum SplitMode {
+    LT, // <
+    LEQ // <=
+  };
+
+  // Split tree at head into two trees, SplitMode decides where EQ values go.
+  // We have SplitMode because it makes remove() trivial to implement.
+  static pair split(Nd* head, const K& key, SplitMode mode = LEQ) {
+    if (head == nullptr) {
+      return {nullptr, nullptr};
+    }
+    if ( (CMP(head->key, key) <= 0 && mode == LEQ) ||
+         (CMP(head->key, key) < 0 && mode == LT) ) {
+      auto p = split(head->right, key, mode);
+      head->right = p.left;
+      return {head, p.right};
+    } else {
+      auto p = split(head->left, key, mode);
+      head->left = p.right;
+      return {p.left, head};
+    }
+  }
+
+  // Invariant: left is a treap whose keys are LEQ to the keys in right.
+  static Nd* merge(Nd* left, Nd* right) {
+    if (left == nullptr) return right;
+    if (right == nullptr) return left;
+
+    if (left->priority > right->priority) {
+      // We need
+      //      LEFT
+      //         |
+      //         RIGHT
+      // For the invariant re: priorities to hold.
+      left->right = merge(left->right, right);
+      return left;
+    } else {
+      // We need
+      //         RIGHT
+      //         |
+      //      LEFT
+      // For the invariant re: priorities to hold.
+      right->left = merge(left, right->left);
+      return right;
+    }
+  }
+
+public:
+  TreapNode(const K& k, const V& v, uint64_t p)
+  : priority(p), key(k), value(v) {
+  }
+
+  static Nd* mk_nd(const K& k, const V& v) {
+    return new Nd(k, v, RAND());
+  }
+
+  static Nd* find(Nd* node, const K& k) {
+    if (node == nullptr) {
+      return nullptr;
+    }
+    if (CMP(node->key, k) == 0) { // EQ
+      return node;
+    }
+
+    if (CMP(node->key, k) <= 0) { // LEQ
+      return find(node->left, k);
+    } else {
+      return find(node->right, k);
+    }
+  }
+
+  static Nd* upsert(Nd* head, const K& k, const V& v) {
+    // (LEQ_k, GT_k)
+    pair split = Nd::split(head, k);
+    Nd* found = find(split.left, k);
+    if (found != nullptr) {
+      // Already exists, update value.
+      found->value = v;
+      return merge(split.left, split.right);
+    }
+    // Doesn't exist, make node
+    Nd* node = mk_nd(k, v);
+    // merge(merge(LEQ_k, EQ_k), GT_k)
+    return merge(merge(split.left, node), split.right);
+  }
+
+  static Nd* remove(Nd *head, const K &k) {
+    // (LEQ_k, GT_k)
+    pair fstSplit = split(head, k, LEQ);
+    // (LT_k, GEQ_k) == (LT_k, EQ_k) since it's from LEQ_k and keys are unique.
+    pair sndSplit = split(fstSplit.left, k, LT);
+
+    if (sndSplit.right != nullptr) {
+      // The key k existed, we delete it.
+      delete sndSplit.right;
+    }
+    // Merge together everything
+    return merge(sndSplit.left, fstSplit.right);
+  }
+
+};
+
+#endif //SHARE_NMT_TREAP_HPP
---
+++ src/hotspot/share/nmt/vmatree.hpp
@@ -0,0 +1,207 @@
+/*
+ * Copyright (c) 2024, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ *
+ */
+
+#ifndef SHARE_NMT_VMATREE_HPP
+#define SHARE_NMT_VMATREE_HPP
+
+#include "utilities/globalDefinitions.hpp"
+#include "nmt/treap.hpp"
+#include "runtime/os.hpp"
+#include "utilities/growableArray.hpp"
+
+int addr_cmp(size_t a, size_t b) {
+  if (a < b) return -1;
+  if (a == b) return 0;
+  if (a > b) return 1;
+  else {
+    // Can't happen
+    return -1337;
+  }
+}
+
+uint64_t wyhash64_x;
+uint64_t wyhash64() {
+  wyhash64_x += 0x60bee2bee120fc15;
+  __uint128_t tmp;
+  tmp = (__uint128_t) wyhash64_x * 0xa3b195354a39b70d;
+  uint64_t m1 = (tmp >> 64) ^ tmp;
+  tmp = (__uint128_t)m1 * 0x1b03738712fad5c9;
+  uint64_t m2 = (tmp >> 64) ^ tmp;
+  return m2;
+}
+
+void* node_malloc(size_t x) {
+  return os::malloc(x, mtNMT);
+}
+
+
+class VMATree {
+  struct State { bool in; bool out; };
+  using VTreap = TreapNode<size_t, State, addr_cmp, wyhash64, node_malloc>;
+  VTreap* tree;
+public:
+  VMATree()
+  : tree(nullptr) {
+  }
+
+  void register_mapping(size_t A, size_t B, bool in_use) {
+    State stA{false, in_use};
+    State stB{in_use, false};
+
+    // First handle A.
+    // Find first node LEQ A
+    VTreap* le_n = nullptr;
+    { // le search
+      VTreap* head = tree;
+      while (head != nullptr) {
+        int cmp_r = addr_cmp(head->key, A);
+        if (cmp_r == 0) { // Exact match
+          le_n = head;
+          break;
+        }
+        if (cmp_r < 0) {
+          // Found a match, try to find a better one.
+          le_n = head;
+          head = head->right;
+        } else {
+          head = head->left;
+        }
+      }
+    }
+    if (le_n == nullptr) {
+      // NO match.
+      if (stA.in == stA.out) {
+        // nothing to do.
+      } else {
+        // Add new node.
+        tree = VTreap::upsert(tree, A, stA);
+      }
+    } else {
+      // Unless we know better, let B's outgoing state be the outgoing state of the node at or preceding A.
+      stB.out = le_n->value.out;
+
+      // Direct address match.
+      if (le_n->key == A) {
+        // Take over in state from old address.
+        stA.in = le_n->value.in;
+
+        // But we may now be able to merge two regions:
+        // If the node's old state matches the new, it becomes a noop. That happens, for example,
+        //  when expanding a committed area: commit [x1, x2); ... commit [x2, x3)
+        //  and the result should be a larger area, [x1..x3). In that case, the middle node (x2)
+        //  is not needed anymore.
+        // So we just remove the old node.
+        if (stA.in == stA.out) {
+          // invalidates le_n
+          tree = VTreap::remove(tree, le_n->key);
+        } else {
+          // re-use existing node
+          le_n->value = stA;
+        }
+      } else {
+        // The address must be smaller.
+
+        // We add a new node, but only if there would be a state change. If there would not be a
+        // state change, we just omit the node.
+        // That happens, for example, when reserving within an already reserved region.
+        stA.in = le_n->value.out; // .. and the region's prior state is the incoming state
+        if (stA.in == stA.out) {
+          // Nothing to do.
+        } else {
+          // Add new node.
+          tree = VTreap::upsert(tree, A, stA);
+        }
+      }
+    }
+
+    // Now we handle B.
+    // We first search all nodes that are between A and B. All of these nodes
+    // need to be deleted. The last node before B determines B's outgoing state.
+    // If there is no node between A and B, its A's incoming state.
+    GrowableArrayCHeap<size_t, mtNMT> to_be_deleted;
+    bool B_needs_insert = true;
+
+    // Find all nodes between (A, B] and record their addresses. Also update B's
+    // outgoing state.
+    auto do_it = [&](VTreap* node) {
+      stB.out = node->value.out;
+      if (node->key < B) {
+        // Delete all nodes preceding B.
+        to_be_deleted.push(node->key);
+      } else {
+        // Re-purpose B node, unless it would result in a noop node, in which case
+        // delete old node at B.
+        if (stB.in == stB.out) {
+          to_be_deleted.push(B);
+        } else {
+          node->value = stB;
+        }
+        B_needs_insert = false;
+      }
+    };
+    { // Iterate over each node who is larger than A
+    GrowableArrayCHeap<VTreap*, mtNMT> to_visit;
+      to_visit.push(tree);
+      VTreap* head = nullptr;
+      while (!to_visit.empty()) {
+        head = to_visit.top();
+        to_visit.pop();
+        if (head == nullptr) continue;
+
+        int cmp_r = addr_cmp(head->key, A);
+        if (cmp_r > 0) {
+          // Do it.
+          do_it(head);
+          // Go both left and right.
+          to_visit.push(head->left);
+          to_visit.push(head->right);
+        } else if (cmp_r >= 0) {
+          // Don't do it.
+          // Go right.
+          to_visit.push(head->right);
+        }
+      }
+    }
+    // Insert B node if needed
+    if (B_needs_insert && !(stB.in == stB.out)) {
+      tree = VTreap::upsert(tree, B, stB);
+    }
+
+    // Finally, if needed, delete all nodes between (A, B)
+    while (to_be_deleted.size() > 0) {
+      const size_t delete_me = to_be_deleted.top();
+      to_be_deleted.pop();
+      tree = VTreap::remove(tree, delete_me);
+    }
+  }
+
+  void register_new_mapping(size_t from, size_t to) {
+    register_mapping(from, to, true);
+  }
+  void register_unmapping(size_t from, size_t to) {
+    register_mapping(from, to, false);
+  }
+};
+
+#endif
