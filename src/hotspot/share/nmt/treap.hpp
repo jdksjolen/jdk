6e49bafbf71481198c5d43f37056cace8a77e6e9
Johan Sjölén <johan.sjolen@oracle.com>
2024-03-04 11:21:48 +0100

    Refactor TreapNode into TreapNode and TreapCHeap

--- src/hotspot/share/nmt/treap.hpp
+++ src/hotspot/share/nmt/treap.hpp
@@ -25,6 +25,9 @@
 #ifndef SHARE_NMT_TREAP_HPP
 #define SHARE_NMT_TREAP_HPP
 
+#include "utilities/globalDefinitions.hpp"
+#include "memory/allocation.hpp"
+#include "runtime/os.hpp"
 #include <stddef.h>
 #include <stdint.h>
 
@@ -38,18 +41,20 @@
 // Recursion is used in these, but the depth of the call stack is the depth of
 // the tree which is O(log n) so we are safe from stack overflow.
 // There is an imperative equivalent of these two, if needed. https://www2.hawaii.edu/~nodari/teaching/f19/scribes/notes07.pdf
-template<typename K, typename V, int(*CMP)(K,K), uint64_t(*RAND)(), void* (*ALLOC)(size_t), void (*FREE)(void*)>
+template<typename K, typename V, int(*CMP)(K,K)>
 class TreapNode {
   template<typename METADATA> // Has to mention the template parameter
-  friend class VMATree;
   friend class VirtualMemoryView;
+
+  template<typename KK, typename VV, int(*CMPP)(KK,KK)> // Just need unique names
+  friend class TreapCHeap;
 
   uint64_t priority;
   K key;
   V value;
-  using Nd = TreapNode<K,V,CMP, RAND, ALLOC, FREE>;
-  TreapNode<K, V, CMP, RAND, ALLOC, FREE>* left;
-  TreapNode<K, V, CMP, RAND, ALLOC, FREE>* right;
+  using Nd = TreapNode<K,V,CMP>;
+  TreapNode<K, V, CMP>* left;
+  TreapNode<K, V, CMP>* right;
 private:
   struct pair {
     Nd* left;
@@ -108,12 +113,6 @@ public:
   : priority(p), key(k), value(v), left(nullptr), right(nullptr) {
   }
 
-  static Nd* mk_nd(const K& k, const V& v) {
-    void* place = ALLOC(sizeof(Nd));
-    new (place) Nd(k, v, RAND());
-    return (Nd*)place;
-  }
-
   static Nd* find(Nd* node, const K& k) {
     if (node == nullptr) {
       return nullptr;
@@ -129,7 +128,8 @@ public:
     }
   }
 
-  static Nd* upsert(Nd* head, const K& k, const V& v) {
+  template<typename MakeNode>
+  static Nd* upsert(Nd* head, const K& k, const V& v, MakeNode make_node) {
     // (LEQ_k, GT_k)
     pair split = Nd::split(head, k);
     Nd* found = find(split.left, k);
@@ -139,12 +139,13 @@ public:
       return merge(split.left, split.right);
     }
     // Doesn't exist, make node
-    Nd* node = mk_nd(k, v);
+    Nd* node = make_node(k, v);
     // merge(merge(LEQ_k, EQ_k), GT_k)
     return merge(merge(split.left, node), split.right);
   }
 
-  static Nd* remove(Nd *head, const K &k) {
+  template<typename Free>
+  static Nd* remove(Nd *head, const K& k, Free free) {
     // (LEQ_k, GT_k)
     pair fstSplit = split(head, k, LEQ);
     // (LT_k, GEQ_k) == (LT_k, EQ_k) since it's from LEQ_k and keys are unique.
@@ -152,12 +153,44 @@ public:
 
     if (sndSplit.right != nullptr) {
       // The key k existed, we delete it.
-      FREE(sndSplit.right);
+      free(sndSplit.right);
     }
     // Merge together everything
     return merge(sndSplit.left, fstSplit.right);
   }
+};
 
+template<typename K, typename V, int(*CMP)(K,K)>
+class TreapCHeap {
+  using CTreap = TreapNode<K, V, CMP>;
+  CTreap* tree;
+  uint64_t prng_seed;
+public:
+  TreapCHeap() : tree(nullptr), prng_seed(1234) {
+  }
+  uint64_t prng_next() {
+    static const uint64_t PrngMult = 0x5DEECE66DLL;
+    static const uint64_t PrngAdd = 0xB;
+    static const uint64_t PrngModPower = 48;
+    static const uint64_t PrngModMask = (static_cast<uint64_t>(1) << PrngModPower) - 1;
+    prng_seed = (PrngMult * prng_seed + PrngAdd) & PrngModMask;
+    return prng_seed;
+  }
+
+  void upsert(const K& k, const V& v) {
+    tree = CTreap::upsert(tree, k, v, [&]() {
+      uint64_t rand = this->prng_next();
+      void* place = os::malloc(sizeof(CTreap), mtNMT);
+      new (place) CTreap(k, v, rand);
+      return (CTreap*)place;
+    });
+  }
+
+  void remove(const K& k) {
+    tree = CTreap::remove(k, [](void* ptr) {
+      os:free(ptr);
+    });
+  }
 };
 
 #endif //SHARE_NMT_TREAP_HPP
