150cb6120d22905d5bed0a33151f6abe889a6732
Johan Sjölén <johan.sjolen@oracle.com>
2024-03-04 12:46:07 +0100

    Test VMATree

---
+++ test/hotspot/gtest/nmt/test_vmatree.cpp
@@ -0,0 +1,19 @@
+#include "precompiled.hpp"
+#include "memory/allocation.hpp"
+#include "nmt/vmatree.hpp"
+#include "nmt/memTracker.hpp"
+#include "runtime/os.hpp"
+#include "unittest.hpp"
+
+TEST_VM(VMATreeTest, AdjacentAreMerged) {
+  struct Nothing{};
+  Nothing nothing;
+  VMATree<Nothing> tree;
+  tree.register_mapping(0, 100, true, nothing);
+  tree.register_mapping(101, 200, true, nothing);
+  int count = 0;
+  tree.visit(0, 300, [&](VMATree<Nothing>::VTreap* x) {
+    count++;
+  });
+  ASSERT_EQ(count, 1);
+}
