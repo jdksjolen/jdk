654334f2e1c1b7740a9586fd3f559fd94a04e639
Johan Sjölén <johan.sjolen@oracle.com>
2024-03-04 15:28:41 +0100

    Comment out VMV

--- src/hotspot/share/nmt/virtualMemoryView.cpp
+++ src/hotspot/share/nmt/virtualMemoryView.cpp
@@ -55,37 +55,37 @@ VirtualMemoryView::PhysicalMemorySpace VirtualMemoryView::Interface::register_sp
 
 void VirtualMemoryView::reserve_memory(PhysicalMemorySpace space, address base_addr, size_t size,
                                        MEMFLAGS flag, const NativeCallStack& stack) {
-  NativeCallStackStorage::StackIndex idx = _stack_storage.push(stack);
-  MetadataReserved md(idx, flag, space, (size_t)base_addr);
-  _virt_mem.reserved_regions.register_new_mapping((size_t)base_addr, (size_t)base_addr+size, md);
+  //NativeCallStackStorage::StackIndex idx = _stack_storage.push(stack);
+  //MetadataReserved md(idx, flag, space, (size_t)base_addr);
+  //_virt_mem.reserved_regions.register_new_mapping((size_t)base_addr, (size_t)base_addr+size, md);
 }
 void VirtualMemoryView::release_memory(address base_addr, size_t size) {
   // No-op metadata
-  MetadataReserved md((size_t)base_addr);
-  _virt_mem.reserved_regions.register_unmapping((size_t)base_addr, (size_t)base_addr+size, md);
+  //MetadataReserved md((size_t)base_addr);
+  //_virt_mem.reserved_regions.register_unmapping((size_t)base_addr, (size_t)base_addr+size, md);
 }
 void VirtualMemoryView::commit_memory_into_space(const PhysicalMemorySpace space, address offset,
                                                  size_t size, const NativeCallStack& stack) {
-  NativeCallStackStorage::StackIndex idx = _stack_storage.push(stack);
-  MetadataOtherSpace md{idx};
-  _virt_mem.committed_regions.at(space.id).register_new_mapping((size_t)offset, (size_t)offset+size, md);
+  //NativeCallStackStorage::StackIndex idx = _stack_storage.push(stack);
+  //MetadataOtherSpace md{idx};
+  //_virt_mem.committed_regions.at(space.id).register_new_mapping((size_t)offset, (size_t)offset+size, md);
 }
 void VirtualMemoryView::uncommit_memory_into_space(const PhysicalMemorySpace& space, address offset,
                                                    size_t size) {
-  MetadataOtherSpace md;
-  _virt_mem.committed_regions.at(space.id).register_unmapping((size_t)offset, (size_t)offset+size, md);
+  //MetadataOtherSpace md;
+  //_virt_mem.committed_regions.at(space.id).register_unmapping((size_t)offset, (size_t)offset+size, md);
 }
 void VirtualMemoryView::add_view_into_space(const PhysicalMemorySpace& space, address base_addr,
                                             size_t size, address offset, MEMFLAGS flag,
                                             const NativeCallStack& stack) {
-  NativeCallStackStorage::StackIndex idx = _stack_storage.push(stack);
-  MetadataReserved md{idx, flag, space, (size_t)offset};
-  _virt_mem.reserved_regions.register_new_mapping((size_t)base_addr, (size_t)base_addr+size, md);
+  //NativeCallStackStorage::StackIndex idx = _stack_storage.push(stack);
+  //MetadataReserved md{idx, flag, space, (size_t)offset};
+  //_virt_mem.reserved_regions.register_new_mapping((size_t)base_addr, (size_t)base_addr+size, md);
 }
 void VirtualMemoryView::remove_view_into_space(const PhysicalMemorySpace& space, address base_addr,
                                                size_t size) {
-  MetadataReserved md;
-  _virt_mem.reserved_regions.register_unmapping((size_t)base_addr, (size_t)base_addr+size, md);
+  //MetadataReserved md;
+  //_virt_mem.reserved_regions.register_unmapping((size_t)base_addr, (size_t)base_addr+size, md);
 }
 
 
@@ -126,7 +126,7 @@ VirtualMemoryView::VirtualMemory::VirtualMemory()
   : reserved_regions(),
     committed_regions(),
     summary() {
-  committed_regions.push(VMATree<MetadataOtherSpace>());
+  //committed_regions.push(VMATree<MetadataOtherSpace>());
 }
 VirtualMemoryView::VirtualMemory::VirtualMemory(const VirtualMemory& other) {
   *this = other;
